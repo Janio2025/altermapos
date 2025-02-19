d44dbfd426d2f660461cb094e191a8623f44f6dc
Janio2025 <githubjanioviana@outlook.com>
2025-02-19 11:33:50 -0300

    criar tabelas organizador

--- updates/update_3.6.0.sql
+++ updates/update_3.6.0.sql
@@ -3,3 +3,4 @@ ALTER TABLE `produtos_os` ADD `descricao` VARCHAR(80) NULL AFTER `quantidade`, A
 ALTER TABLE `servicos_os` ADD `servico` VARCHAR(80) NULL AFTER `idServicos_os`, ADD `quantidade` DOUBLE NULL AFTER `servico`, ADD `preco` VARCHAR(15) NULL AFTER `quantidade`;
 
 ALTER TABLE `itens_de_vendas` ADD `preco` VARCHAR(15) NULL AFTER `quantidade`;
+
