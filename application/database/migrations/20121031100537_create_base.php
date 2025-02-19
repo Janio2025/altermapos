292edfef7e956fe3f4ab13edda63ce2c31ffee27
Janio2025 <githubjanioviana@outlook.com>
2025-02-19 17:16:48 -0300

    moduanças

--- application/database/migrations/20121031100537_create_base.php
+++ application/database/migrations/20121031100537_create_base.php
@@ -324,63 +324,6 @@ class Migration_create_base extends CI_Migration
         $this->dbforge->create_table('permissoes', true);
         $this->db->query('ALTER TABLE  `permissoes` ENGINE = InnoDB');
 
-         //# Create Table organizadores
-         $this->dbforge->add_field([
-            'id' => [
-                'type' => 'INT',
-                'constraint' => 11,
-                'null' => false,
-                'auto_increment' => true,
-            ],
-            'nome_organizador' => [
-                'type' => 'VARCHAR',
-                'constraint' => 255,
-                'null' => false,
-            ],
-            'localizacao' => [
-                'type' => 'VARCHAR',
-                'constraint' => 255,
-                'null' => true,
-            ],
-            'ativa' => [
-                'type' => 'BOOLEAN',
-                'default' => true,
-                'null' => false,
-            ],
-        ]);
-        $this->dbforge->add_key('id', true);
-        $this->dbforge->create_table('organizadores', true);
-        $this->db->query('ALTER TABLE `organizadores` ENGINE = InnoDB'); 
-
-          //# Create Table compartimentos
-          $this->dbforge->add_field([
-            'id' => [
-                'type' => 'INT',
-                'constraint' => 11,
-                'null' => false,
-                'auto_increment' => true,
-            ],
-            'organizador_id' => [
-                'type' => 'INT',
-                'constraint' => 11,
-                'null' => false,
-            ],
-            'nome_compartimento' => [
-                'type' => 'VARCHAR',
-                'constraint' => 255,
-                'null' => false,
-            ],
-            'ativa' => [
-                'type' => 'BOOLEAN',
-                'default' => true,
-                'null' => false,
-            ],
-        ]);
-        $this->dbforge->add_key('id', true);
-        $this->dbforge->add_field('CONSTRAINT FOREIGN KEY (organizador_id) REFERENCES organizadores(id) ON DELETE CASCADE');
-        $this->dbforge->create_table('compartimentos', true);
-        $this->db->query('ALTER TABLE `compartimentos` ENGINE = InnoDB');  
-
         //# Create Table usuarios
         $this->dbforge->add_field([
             'idUsuarios' => [
@@ -1370,8 +1313,6 @@ class Migration_create_base extends CI_Migration
         $this->dbforge->add_key('idAnotacoes', true);
         $this->dbforge->create_table('anotacoes_os', true);
         $this->db->query('ALTER TABLE `anotacoes_os` ENGINE = InnoDB');
-
-
 
         //# Create Table configuracoes
         $this->dbforge->add_field([
