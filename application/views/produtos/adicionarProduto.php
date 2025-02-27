0ed2e49245f3299d3dcd525810496bcf9b766c72
Janio2025 <githubjanioviana@outlook.com>
2025-02-26 22:48:23 -0300

    teste preço mobile

--- application/views/produtos/adicionarProduto.php
+++ application/views/produtos/adicionarProduto.php
@@ -624,3 +624,20 @@
         }
     }
 </script>
+
+<script type="text/javascript">
+    function corrigirPrecoMobile() {
+        if (window.matchMedia('(max-width: 768px)').matches) {
+            document.querySelectorAll('.money').forEach(function(input) {
+                input.addEventListener('input', function() {
+                    let valor = this.value.replace(/,/g, '.');
+                    if (!isNaN(valor) && valor !== '') {
+                        this.value = parseFloat(valor).toFixed(2);
+                    }
+                });
+            });
+        }
+    }
+    
+    document.addEventListener("DOMContentLoaded", corrigirPrecoMobile);
+</script>
